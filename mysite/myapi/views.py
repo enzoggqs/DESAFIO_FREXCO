9c4449fcbb68e36c55a1182c14d8ebb860e69906
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-01-17 13:25:16 -0300

    Creating views

--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# views.py
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import RegionSerializer, FruitSerializer
+from .models import Region, Fruit
+
+class RegionViewSet(viewsets.ModelViewSet):
+    queryset = Region.objects.all().order_by('name')
+    serializer_class = RegionSerializer
+
+class FruitViewSet(viewsets.ModelViewSet):
+    queryset = Fruit.objects.all().order_by('name')
+    serializer_class = FruitSerializer
