7206e460b1295e56e51d6aaa9c428e7d6858d80e
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-01-17 13:14:39 -0300

    creating models and applying migrations

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -1,3 +1,13 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=15)
+    region = models.ForeignKey(Region,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
